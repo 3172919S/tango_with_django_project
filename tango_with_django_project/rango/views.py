827505ec4e32ec84a401dde6c6b07b3a021c30d7
“Eilidh <3172919S@student.gla.ac.uk>
2026-03-03 14:50:51 +0000

    Chapter 6, exercises complete

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -11,7 +11,24 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    
     return render(request, 'rango/index.html', context=context_dict)
 
 def about (request) :
     return render(request, 'rango/index.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
+
